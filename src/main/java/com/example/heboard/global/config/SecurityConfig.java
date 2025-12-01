722c7e0962be96f0295035ce1b45c18e77357fed
randirao <randirao4245@gmail.com>
2025-12-01 18:00:18 +0900

    fix: 보안 문제 수정

--- src/main/java/com/example/heboard/global/config/SecurityConfig.java
+++ src/main/java/com/example/heboard/global/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
