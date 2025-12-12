a8241038aac4aa5c5b5e619aa3808c5ca2e33598
randirao <randirao4245@gmail.com>
2025-12-13 00:29:40 +0900

    fix: 게시글 업로드 시간 이슈 수정

--- src/main/java/com/example/heboard/HeboardApplication.java
+++ src/main/java/com/example/heboard/HeboardApplication.java
@@ -1,10 +1,18 @@
 package com.example.heboard;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HeboardApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HeboardApplication.class, args);
